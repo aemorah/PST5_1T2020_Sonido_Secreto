0f6dd242c38f57b8d2f52f642a995da325e6a2f2
Alex Mora Hurel <Aleemora@espol.edu.ec>
2020-08-29 17:40:02 -0500

    Se completo las opciones para el menu de admin

---
+++ app/src/main/java/com/example/smartsound/DesbloquearPuerta.java
@@ -0,0 +1,14 @@
+package com.example.smartsound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DesbloquearPuerta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desbloquear_puerta);
+    }
+}
--- app/src/main/java/com/example/smartsound/MenuIngreso.java
+++ app/src/main/java/com/example/smartsound/MenuIngreso.java
@@ -4,25 +4,50 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.smartsound.model.GuardadoUsuario;
+import com.example.smartsound.model.Persona;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MenuIngreso extends AppCompatActivity {
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+    TextView tv1;
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_ingreso);
         inicializarFirebase();
+
+        tv1=findViewById(R.id.tituloBien);
+        databaseReference.child(GuardadoUsuario.usuarioUsando).child("Datos").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Persona p= snapshot.getValue(Persona.class);
+                assert p != null;
+                tv1.setText("Bienvenido "+ p.getNombre());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
     }
 
     private void inicializarFirebase(){
@@ -58,6 +83,34 @@ public class MenuIngreso extends AppCompatActivity {
             alert.show();
         }
         return true;
+    }
+
+    public void hablar(View view){
+        Intent i = new Intent(this, DesbloquearPuerta.class );
+        startActivity(i);
+
+
+    }
+
+    public void adminDispo(View view){
+        Intent i = new Intent(this, AdministrarDispo.class );
+        startActivity(i);
+
+
+    }
+
+    public void registraUser(View view){
+        Intent i = new Intent(this, RegistrarUsuario.class );
+        startActivity(i);
+
+
+    }
+
+    public void adminUser(View view){
+        Intent i = new Intent(this, AdministrarUsuarios.class );
+        startActivity(i);
+
+
     }
 
 }
